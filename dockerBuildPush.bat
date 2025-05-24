5768f8b42e082a6134c6347d61cf3c410d97a60e
Mattias Wiberg <mattiaswiberg@outlook.com>
2025-05-24 20:15:51 +0200

    Update storage size for PersistentVolumeClaim in Kubernetes manifests and use HTTPS

--- dockerBuildPush.bat
+++ dockerBuildPush.bat
@@ -1,6 +1,6 @@
 docker build -t 192.168.10.153:32000/timelaps-service:latest ./timelaps-service
-docker build --build-arg VITE_API_BASE_URL=http://192.168.10.153/api -t 192.168.10.153:32000/camera-frontend:latest ./camera-frontend
-docker build --build-arg VITE_API_BASE_URL=http://192.168.10.153/api -t 192.168.10.153:32000/timelaps-frontend:latest ./timelaps-frontend
+docker build --build-arg VITE_API_BASE_URL=https://192.168.10.153/api -t 192.168.10.153:32000/camera-frontend:latest ./camera-frontend
+docker build --build-arg VITE_API_BASE_URL=https://192.168.10.153/api -t 192.168.10.153:32000/timelaps-frontend:latest ./timelaps-frontend
 docker push 192.168.10.153:32000/timelaps-service:latest
 docker push 192.168.10.153:32000/camera-frontend:latest
 docker push 192.168.10.153:32000/timelaps-frontend:latest
