91be65a8190d9c2bbff99d3fdafd4208d886e3e1
Mattias Wiberg <mattiaswiberg@outlook.com>
2025-05-24 18:49:49 +0200

    add service too

--- dockerBuildPush.bat
+++ dockerBuildPush.bat
@@ -1,4 +1,6 @@
+docker build -t 192.168.10.153:32000/timelaps-service:latest ./timelaps-service
 docker build -t 192.168.10.153:32000/camera-frontend:latest ./camera-frontend
 docker build -t 192.168.10.153:32000/timelaps-frontend:latest ./timelaps-frontend
+docker push 192.168.10.153:32000/timelaps-service:latest
 docker push 192.168.10.153:32000/camera-frontend:latest
 docker push 192.168.10.153:32000/timelaps-frontend:latest
