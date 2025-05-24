64a659a13aaa84f22845ee5839e23785b9436be8
Mattias Wiberg <mattiaswiberg@outlook.com>
2025-05-24 21:03:23 +0200

    Changed to WAN IP

--- dockerBuildPush.bat
+++ dockerBuildPush.bat
@@ -1,6 +1,6 @@
 docker build -t 192.168.10.153:32000/timelaps-service:latest ./timelaps-service
-docker build --build-arg VITE_API_BASE_URL=https://192.168.10.153/api -t 192.168.10.153:32000/camera-frontend:latest ./camera-frontend
-docker build --build-arg VITE_API_BASE_URL=https://192.168.10.153/api -t 192.168.10.153:32000/timelaps-frontend:latest ./timelaps-frontend
 docker push 192.168.10.153:32000/timelaps-service:latest
+docker build --build-arg VITE_API_BASE_URL=https://84.217.51.153/api -t 192.168.10.153:32000/camera-frontend:latest ./camera-frontend
 docker push 192.168.10.153:32000/camera-frontend:latest
+docker build --build-arg VITE_API_BASE_URL=https://84.217.51.153/api -t 192.168.10.153:32000/timelaps-frontend:latest ./timelaps-frontend
 docker push 192.168.10.153:32000/timelaps-frontend:latest
